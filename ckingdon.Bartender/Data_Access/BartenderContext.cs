ca4affce4d0b134698640b60b2ccc38773380e62
ckchessmaster <Genesis1:1>
2017-08-30 15:19:30 -0400

    missed some files from last commit

---
+++ ckingdon.Bartender/Data_Access/BartenderContext.cs
@@ -0,0 +1,26 @@
+﻿using ckingdon.Bartender.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace ckingdon.Bartender.Data_Access
+{
+    public class BartenderContext : DbContext
+    {
+        public BartenderContext() : base("BartenderContext") { }
+
+        public DbSet<Drink> Drinks { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        //public DbSet<Order> ProductionQueue { get; set; }
+        //public DbSet<Order> PickupQueue { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }//end OnModelCreating
+
+    }//end class BartenderContext
+}//end namespace
---
+++ ckingdon.Bartender/Data_Access/BartenderInitializer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using ckingdon.Bartender.Models;
+
+namespace ckingdon.Bartender.Data_Access
+{
+    public class BartenderInitializer : DropCreateDatabaseIfModelChanges<BartenderContext>
+    {
+        //Seed the database with data
+        protected override void Seed(BartenderContext context)
+        {
+            var drinks = new List<Drink>
+            {
+                new Drink {Name="Sex on the Beach", Image="" },
+                new Drink {Name="Whiskey Sour", Image="" },
+                new Drink {Name="Screwdriver", Image="" },
+                new Drink {Name="Margarita", Image="" },
+                new Drink {Name="Martini", Image="" },
+                new Drink {Name="Mojito", Image="" },
+                new Drink {Name="Gin and Tonic", Image="" },
+                new Drink {Name="House IPA", Image="" },
+                new Drink {Name="House Lager", Image="" }
+            };
+
+            drinks.ForEach(d => context.Drinks.Add(d));
+            context.SaveChanges();
+        }//end Seed
+
+    }//end class BartenderInitializer
+}//end namespace
