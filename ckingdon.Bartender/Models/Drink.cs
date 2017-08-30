f0234afb5db6484035ce5c050b99fe3dfcd65ff9
ckchessmaster <Genesis1:1>
2017-08-30 13:38:26 -0400

    SETUP

--- ckingdon.Bartender/Models/Drink.cs
+++ ckingdon.Bartender/Models/Drink.cs
@@ -7,5 +7,9 @@ namespace ckingdon.Bartender.Models
 {
     public class Drink
     {
-    }
-}
+        //Primary Key
+        public int DrinkID { get; set; }
+        public String Name { get; set; }
+        public String Image { get; set; }
+    }//end class Drink
+}//end namespace
--- ckingdon.Bartender/Models/Order.cs
+++ ckingdon.Bartender/Models/Order.cs
@@ -7,5 +7,14 @@ namespace ckingdon.Bartender.Models
 {
     public class Order
     {
-    }
-}
+        //Primary Key
+        public int OrderID { get; set; }
+
+        //Foreign Key
+        public virtual Drink Drink { get; set; }
+
+        public DateTime TimeOrdered { get; set; }
+        public String Customer { get; set; }
+        public int CustomerPIN { get; set; }
+    }//end Class Order
+}//end namespace
